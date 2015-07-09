c121e310802e921d69358e98593aefaa425aa949
Jacobth <jacob.thureson@gmail.com>
2015-07-09 19:45:16 +0200

    first

---
+++ desktop/src/com/eversong/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.eversong.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.eversong.game.eversong;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new eversong(), config);
+	}
+}
